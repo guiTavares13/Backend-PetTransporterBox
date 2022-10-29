99a07e12fc0e53770f11e4494da5910b336f3155
Yago-Caetano <46303870+Yago-Caetano@users.noreply.github.com>
2022-10-29 16:38:50 -0300

    fix - port selection

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('@google-cloud/debug-agent').start({serviceContext: {enableCanary: true}
 const app = require('./src/app');
 
 
-const port = process.env.PORT | 3333
+const port = process.env.PORT || 3333
 app.listen(port,()=>{
     console.log(`PET Transport Backend Running on port: ${port}!!`);
 });
