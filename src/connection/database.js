a681d518c34aa2e0f2f253376b4c98511e16d0bd
Yago-Caetano <46303870+Yago-Caetano@users.noreply.github.com>
2022-10-30 12:09:49 -0300

    refc - database connection

--- src/connection/database.js
+++ src/connection/database.js
@@ -1,19 +1,5 @@
-/*const mysql = require('mysql2/promise');
 
-require('dotenv/config');
-
-var pool = mysql.createPool({
-    host     : process.env.DATABASE_HOST ? process.env.DATABASE_HOST : 'localhost',
-    port:  process.env.DATABASE_PORT ? process.env.DATABASE_PORT : '3306',
-    user     : process.env.DATABASE_USER ? process.env.DATABASE_USER : 'root',
-    password : process.env.DATABASE_PASS ? process.env.DATABASE_PASS : 'root',
-    database : process.env.DATABASE_SCHEMA ? process.env.DATABASE_SCHEMA  : 'default'
-  });
-   
-  exports.pool = pool;
-*/
-  
-const { Sequelize } = require('sequelize');
+/*const { Sequelize } = require('sequelize');
 
 require('dotenv/config');
 
@@ -27,6 +13,28 @@ const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
   //passar os dados para o sequelize
   dialect: "mysql", //informar o tipo de banco que vamos utilizar
   host: dbHost, //o host, neste caso estamos com um banco local
+});
+
+module.exports = sequelize;
+*/
+
+//GCP SQL Intranet connection
+
+require('dotenv/config');
+
+const dbName = process.env.DATABASE_SCHEMA;
+const dbUser = process.env.DATABASE_USER;
+const dbHost = `/cloudsql/${process.env.DATABASE_GCP_INSTANCE}`
+const dbPassword = process.env.DATABASE_PASS;
+
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  //passar os dados para o sequelize
+  dialect: "mysql", //informar o tipo de banco que vamos utilizar
+  host: dbHost, //o host, neste caso estamos com um banco local
+  dialectOptions:{
+    socketPath: `/cloudsql/${DATABASE_GCP_INSTANCE}`
+  }
 });
 
 module.exports = sequelize;
